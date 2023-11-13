0c96c5d007e24c4ea5f076eeddddff6354d224ff
Kevin Kidd <harrison.kevin@protonmail.com>
2023-11-13 17:29:41 -0500

    Fix

--- src/hooks/useInventory.ts
+++ src/hooks/useInventory.ts
@@ -4,7 +4,6 @@ import { useWalletStore } from "../stores/walletStore";
 
 export const useInventory = () => {
   const { address, client, permit } = useWalletStore((state) => state);
-  
   return useQuery(["inventory"], async () => {
     if (!client.value) throw new Error("Failed to connect to wallet");
     if (!permit.value) throw new Error("Failed to get permit");
